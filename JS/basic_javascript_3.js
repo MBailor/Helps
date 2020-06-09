4d9f353ce7e348f248ad7a0acecabf4e96dbb36f
MBailor <65557271+MBailor@users.noreply.github.com>
2020-06-09 10:10:52 -0700

    Folder for things I need help with

---
+++ JS/basic_javascript_3.js
@@ -0,0 +1,17 @@
+myFunction();
+
+function myFunction() {
+  var carName = "Volvo";
+  document.getElementById("demo1").innerHTML =
+  typeof carName + " " + carName;
+}
+
+document.getElementById("demo2").innerHTML =
+typeof carName.getElementById()
+
+more_Math();
+
+function more_Math() {
+  var simple_Math = (1+2) * 10 / 2-5;
+  document.getElementById("Math4").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals" + simple_Math;
+}
