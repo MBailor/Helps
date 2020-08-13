d218ed161c69195c8f53587bf1c451e5cc6d6965
MBailor <65557271+MBailor@users.noreply.github.com>
2020-08-12 20:01:31 -0700

    Update TkTry.py
    pushing for help

--- TkTry.py
+++ TkTry.py
@@ -14,12 +14,12 @@ class ParentWin(Frame):
         self.master.protocol("WM_DELETE_WINDOW")
         arg = self.master
 
-        self.b1 = Button(self.master, text="Browse", width=12).grid(row=3, column=0, padx=10, pady=10, sticky=W)
-        self.e1 = Entry(self.master,text='').grid(row=3, column=1, columnspan=10, padx=10, pady=10, sticky=N+E)
-        self.b2 = Button(self.master, text="Browse", width=12).grid(row=4, column=0, padx=10, pady=10, sticky=W)
-        self.e2 = Entry(self.master, text='').grid(row=4, column=1, columnspan=10, padx=10, pady=10, sticky=N+E)
-        self.b3 = Button(self.master, text="Check For Files...", height=2).grid(row=7, column=0, rowspan=3, padx=10, pady=10, sticky=W)
-        self.b3 = Button(self.master, text="Close Program", height=2).grid(row=7, column=10, padx=10, pady=10, sticky=E)
+        self.b1 = Button(self.master, text="Browse", width=12).grid(row=3, column=0, padx=10, pady=10, sticky=W+S)
+        self.e1 = Entry(self.master,text='', width="35").grid(row=3, column=1, columnspan=10, padx=10, pady=10, sticky=S+E)
+        self.b2 = Button(self.master, text="Browse", width=12).grid(row=4, column=0, padx=10, pady=0, sticky=W)
+        self.e2 = Entry(self.master, text='', width="35").grid(row=4, column=1, columnspan=10, padx=10, pady=0, sticky=N+E)
+        self.b3 = Button(self.master, text="Check For Files...", height=2).grid(row=7, column=0, rowspan=3, padx=10, pady=10, sticky=S+W)
+        self.b3 = Button(self.master, text="Close Program", height=2).grid(row=7, column=10, padx=10, pady=10, sticky=S+E)
 
 
 
