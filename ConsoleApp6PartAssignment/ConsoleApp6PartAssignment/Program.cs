cec5e7b0e77b1a707d50713945397ec72b36bb34
MBailor <65557271+MBailor@users.noreply.github.com>
2020-09-22 13:28:32 -0700

    6 Part Console App part 5
    Pushing for help

--- ConsoleApp6PartAssignment/ConsoleApp6PartAssignment/Program.cs
+++ ConsoleApp6PartAssignment/ConsoleApp6PartAssignment/Program.cs
@@ -47,35 +47,89 @@ namespace ConsoleApp6PartAssignment
             //Console.ReadLine();
 
 
-                List<string> ranStrings = new List<string>();
-                ranStrings.Add("Pancakes");
-                ranStrings.Add("French Toast");
-                ranStrings.Add("Chicken Fried Steak");
-                ranStrings.Add("Bacon");
-                ranStrings.Add("Oatmeal");
-                ranStrings.Add("Fruit Cup");
+            //List<string> ranStrings = new List<string>();
+            //ranStrings.Add("Pancakes");
+            //ranStrings.Add("French Toast");
+            //ranStrings.Add("Chicken Fried Steak");
+            //ranStrings.Add("Bacon");
+            //ranStrings.Add("Oatmeal");
+            //ranStrings.Add("Fruit Cup");
 
-                Console.WriteLine("Please enter a Breakfast item to see if it's available");
-                string entFood = Console.ReadLine();
-                string ranStrings1 = Convert.ToString(ranStrings);
-                int getFoodIndex = ranStrings.FindIndex(s => String.Equals(s, entFood));
-                bool getFoodString = Convert.ToBoolean(getFoodIndex);
+            //Console.WriteLine("Please enter a Breakfast item to see if it's available");
+            //string entFood = Console.ReadLine();
+            //string ranStrings1 = Convert.ToString(ranStrings);
+            //int getFoodIndex = ranStrings.FindIndex(s => String.Equals(s, entFood));
+            //bool getFoodString = Convert.ToBoolean(getFoodIndex);
+            //bool isMatch = false;
+
+            //do
+            //{
+            //    if (ranStrings.Contains(entFood))
+            //    {
+            //        Console.WriteLine("Yum! You chose: " + entFood + " Excellant choice! This is Item #: " + getFoodIndex + "");
+            //        isMatch = true;
+            //        Console.ReadLine();
+            //        return;
+            //    }
+            //    else if (!ranStrings.Contains(entFood))
+            //    {
+            //        Console.WriteLine("You have selected a food item that is unavailable.");
+            //        Console.WriteLine("Please enter a Breakfast item to see if it's available");
+            //        entFood = Console.ReadLine();
+            //        ranStrings1 = Convert.ToString(ranStrings);
+            //        getFoodIndex = ranStrings.FindIndex(s => String.Equals(s, entFood));
+            //        getFoodString = Convert.ToBoolean(getFoodIndex);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Yum! You chose: " + entFood + " Excellant choice! This is Item #: " + getFoodIndex + "");
+            //        isMatch = true;
+            //        Console.ReadLine();
+            //        return;
+            //    }
+            //} while (isMatch == false);
 
 
 
-                if (ranStrings.Contains(entFood))
+            List<string> officeSupplies = new List<string>();
+            officeSupplies.Add("Pencil");
+            officeSupplies.Add("Pen");
+            officeSupplies.Add("Scissor");
+            officeSupplies.Add("Hole Punch");
+            officeSupplies.Add("Pen");
+            officeSupplies.Add("Paper");
+
+            Console.WriteLine("Please enter an office supply item to see if it's available");
+            string entSearch = Console.ReadLine();
+            string stringSearch1 = Convert.ToString(officeSupplies);
+            int getStringIndex = officeSupplies.FindIndex(s => String.Equals(s, entSearch));
+            bool getIndexString = Convert.ToBoolean(getStringIndex);
+            bool isAMatch = false;
+            int index = 0;
+
+            
+            
+
+                foreach (string supply in officeSupplies)
                 {
-                    Console.WriteLine("Yum! You chose: " + entFood + " Excellant choice! This is Item #: " + getFoodIndex + "");
+                    Console.WriteLine($"{index}: {supply}");
+                    
+
+                    if (officeSupplies.Contains(entSearch))
+                    {
+
+                        Console.WriteLine("You have chosen the office supply, " + entSearch + "Which has an idex of:" + getStringIndex + "");
+                        Console.WriteLine("You have chosen the office supply, " + entSearch + " Which has an idex of:" + getStringIndex + "");
+                        index++;
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You have chosen an office supply that is currently unavailable.");
+                        Console.ReadLine();
+                    }
                 }
-                else 
-                {
-                    Console.WriteLine("You have selected a food item that is unavailable.");
-                    Console.WriteLine("Please enter a Breakfast item to see if it's available");
-                    entFood = Console.ReadLine();
-                    ranStrings1 = Convert.ToString(ranStrings);
-                }
-            Console.ReadLine();
+
         }
-
     }
 }
