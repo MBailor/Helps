eafb209f98794968d944b62cb9d411c25d9a1bb4
MBailor <65557271+MBailor@users.noreply.github.com>
2020-09-25 13:28:11 -0700

    Pushing for help calling a method from one .cs to the main program
    help

---
+++ MethodsAssignment/MethodsAssignment/Math-ods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    public class Math_ods
+    {
+        public Math_ods()
+        {
+            int Math_od1 = 43;
+            int Math_od2 = 89;
+            int Math_od3 = 12;
+        }
+        public int Math_od1 { get; set; }
+        public int Math_od2 { get; set; }
+        public int Math_od3 { get; set; }
+    }
+    
+}
---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+
+        public static void Math_ods()
+        { 
+
+
+            Console.WriteLine("Welcome to Math Time!\n\n The Show Where You Do\n\n Math With A Secret Number!!!");
+            Console.ReadLine();
+            Console.WriteLine("Let's get this game started!\n\n Pleace enter a number you'd\n\n Like to multiply!");
+            Console.ReadLine();
+            string entNums = Console.ReadLine();
+            int entNum = Convert.ToInt32(entNums);
+             
+            
+            
+            Console.WriteLine("Your number, " + entNum + "Time the secreat number is;");
+            Console.ReadLine();
+
+            Console.WriteLine("Your answer is..........");
+            Console.WriteLine(Math_od1 * entNum);
+            Console.ReadLine();
+
+        }
+        Console.ReadLine();
+     } }  
+    }
+} 
